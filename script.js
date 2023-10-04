422c957522d06e0a97c391e243e7856c8df747fa
mhide1010 <mhideexchange@gmail.com>
2023-10-03 22:16:07 -0400

    calculatortwo

---
+++ script.js
@@ -0,0 +1,8 @@
+const west =['ilorin', 'osun']
+const north =['nasarawa', 'benue', 'lafia']
+
+const output =document.getElementById('output').innerHTML=(north)
+const cd =document.getElementById('output2').innerHTML=(west)
+
+// console.log(west)
+// console.log(north)   
